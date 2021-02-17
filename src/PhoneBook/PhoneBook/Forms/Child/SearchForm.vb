aca497e04dcb0e8175be425b16d3e4c07a309835
Philip Shishov <philshishov@gmail.com>
2021-02-17 23:37:05 +0100

    Finish phone book project with VB and WinForms

---
+++ src/PhoneBook/PhoneBook/Forms/Child/SearchForm.vb
@@ -0,0 +1,22 @@
+﻿Public Class SearchForm
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim fileName As String = "D:\Downloads\" + Me.TextBox9.Text + "_info.txt"
+
+        If System.IO.File.Exists(fileName) = True Then
+            Me.TextBox1.Text = Me.TextBox9.Text
+
+            Dim text As String =
+            System.IO.File.ReadAllText(fileName, System.Text.Encoding.UTF8)
+
+            Dim arrString() As String = Split(text, Environment.NewLine)
+
+            Me.TextBox2.Text = arrString(0)
+            Me.TextBox3.Text = arrString(1)
+            Me.TextBox4.Text = arrString(2)
+        Else
+            Me.TextBox1.Text = "Not Found"
+        End If
+
+    End Sub
+End Class
---
+++ src/PhoneBook/PhoneBook/Forms/Main/PhoneBookForm.vb
@@ -0,0 +1,44 @@
+﻿Public Class PhoneBookForm
+    Private Sub ToolStripButton1_Click(sender As Object, e As EventArgs) Handles ToolStripButton1.Click
+        My.Forms.RegisterDataForm.MdiParent = Me
+        My.Forms.RegisterDataForm.Show()
+    End Sub
+
+    Private Sub ToolStripButton2_Click(sender As Object, e As EventArgs) Handles ToolStripButton2.Click
+        Dim form As New SearchForm With {
+            .MdiParent = Me
+        }
+        form.Show()
+    End Sub
+
+    Private Sub ToolStripButton3_Click(sender As Object, e As EventArgs) Handles ToolStripButton3.Click
+        Me.Process1.StartInfo.FileName = "C:\Windows\System32\mspaint.exe"
+        Me.Process1.Start()
+    End Sub
+
+    Private Sub ToolStripButton4_Click(sender As Object, e As EventArgs) Handles ToolStripButton4.Click
+        Dim form As New AboutForm With {
+            .MdiParent = Me
+        }
+        form.Show()
+    End Sub
+
+    Private Sub ToolStripButton5_Click(sender As Object, e As EventArgs) Handles ToolStripButton5.Click
+        MessageBox.Show("Have a nice day!")
+        Me.Close()
+    End Sub
+
+    Private Sub PhoneBookForm_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+        If e.KeyCode = Keys.F2 Then
+            ToolStripButton1_Click(sender, e)
+        ElseIf e.KeyCode = Keys.F3 Then
+            ToolStripButton2_Click(sender, e)
+        ElseIf e.KeyCode = Keys.F4 Then
+            ToolStripButton3_Click(sender, e)
+        ElseIf e.KeyCode = Keys.F5 Then
+            ToolStripButton4_Click(sender, e)
+        ElseIf e.KeyCode = Keys.Escape Then
+            ToolStripButton5_Click(sender, e)
+        End If
+    End Sub
+End Class
