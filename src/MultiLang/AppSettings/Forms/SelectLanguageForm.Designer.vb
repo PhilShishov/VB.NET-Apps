d30dae522d0f99adbb4c8a9d88fca9cb04036196
Philip Shishov <philshishov@gmail.com>
2021-04-07 21:26:06 +0200

    Complete AppSettings and finish Multi Language project

---
+++ src/MultiLang/AppSettings/Forms/SelectLanguageForm.Designer.vb
@@ -0,0 +1,83 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class SelectLanguageForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 15
+        Me.ListBox1.Items.AddRange(New Object() {"English (Default)", "French", "German", "Arabic", "Chinese (Simplified)"})
+        Me.ListBox1.Location = New System.Drawing.Point(7, 36)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(234, 169)
+        Me.ListBox1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(7, 14)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(125, 15)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Select your language:"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(39, 220)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(166, 33)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Change Language"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'select_language
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.ClientSize = New System.Drawing.Size(248, 269)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(178, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "select_language"
+        Me.ShowIcon = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Select Language"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+End Class
---
+++ src/MultiLang/AppSettings/Forms/SelectLanguageForm.vb
@@ -0,0 +1,70 @@
+﻿Public Class SelectLanguageForm
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        '------------------------
+        If Me.ListBox1.SelectedIndex = -1 Then
+            MessageBox.Show(My.Resources.all_strings.frm1_Please_select_your_language)
+            Exit Sub
+        End If
+        '------------------------
+        Dim msg As String
+        msg = My.Resources.all_strings.frm1_Do_you_want_to_close_your_application
+        If MessageBox.Show(msg, My.Resources.all_strings.frm1_Alert, MessageBoxButtons.OKCancel) = DialogResult.Cancel Then
+            Exit Sub
+        End If
+        '------------------------
+        Dim sl As String
+        sl = "EN_DEFAULT"
+        '----------------------------------
+        If Me.ListBox1.SelectedIndex = 0 Then
+            sl = "EN_DEFAULT"
+        End If
+        '------------
+        If Me.ListBox1.SelectedIndex = 1 Then
+            sl = "fr"
+        End If
+        '------------
+        If Me.ListBox1.SelectedIndex = 2 Then
+            sl = "de"
+        End If
+        '------------
+        If Me.ListBox1.SelectedIndex = 3 Then
+            sl = "ar"
+        End If
+        '------------
+        If Me.ListBox1.SelectedIndex = 4 Then
+            sl = "zh-Hans"
+        End If
+        '------------------------------------
+        My.Settings.selected_language = sl
+        My.Settings.Save()
+        '------------------
+        Me.DialogResult = DialogResult.OK
+        'Application.Exit()
+    End Sub
+
+    Private Sub select_language_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim sl As String
+        sl = My.Settings.selected_language
+        '---------------------
+        If sl = "EN_DEFAULT" Then
+            Me.ListBox1.SelectedIndex = 0
+        End If
+        '-----------
+        If sl = "fr" Then
+            Me.ListBox1.SelectedIndex = 1
+        End If
+        '-----------
+        If sl = "de" Then
+            Me.ListBox1.SelectedIndex = 2
+        End If
+        '-----------
+        If sl = "ar" Then
+            Me.ListBox1.SelectedIndex = 3
+        End If
+        '-----------
+        If sl = "zh-Hans" Then
+            Me.ListBox1.SelectedIndex = 4
+        End If
+        '-----------
+    End Sub
+End Class
