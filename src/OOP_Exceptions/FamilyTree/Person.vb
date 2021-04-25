6e4436095afb78661b9424e06e001093a8c1d144
Philip Shishov <philshishov@gmail.com>
2021-04-25 12:46:25 +0200

    FamilyTree VB.NET OOP Recursion

---
+++ src/OOP_Exceptions/FamilyTree/Person.vb
@@ -0,0 +1,18 @@
+﻿Public Class Person
+
+    Public Father As Person
+
+    Public Mother As Person
+
+    Dim name As String
+
+    Public Sub New(name As String, father As Person, mother As Person)
+        Me.name = name
+        Me.Father = father
+        Me.Mother = mother
+    End Sub
+
+    Public Overrides Function ToString() As String
+        Return name
+    End Function
+End Class
---
+++ src/OOP_Exceptions/FamilyTree/Program.vb
@@ -0,0 +1,22 @@
+﻿Module Program
+    Sub Main()
+        ' Create and link people
+        Dim abraham As Person = New Person("Abraham Simpson", Nothing, Nothing)
+        Dim penelope As Person = New Person("Penelope Olsen", Nothing, Nothing)
+        Dim pan As Person = New Person("Pan Bouvier", Nothing, Nothing)
+        Dim jackie As Person = New Person("Jackie Bouvier", Nothing, Nothing)
+        Dim herb As Person = New Person("Herb Powers", abraham, penelope)
+        Dim homer As Person = New Person("Homer Simpson", abraham, penelope)
+        Dim marge As Person = New Person("Marge Bouvier", pan, jackie)
+        Dim selma As Person = New Person("Selma Bouvier", pan, jackie)
+        Dim bart As Person = New Person("Bart Simpson", homer, marge)
+        ' Create and print the family trees
+        Dim firstFamilyTree As FamilyTree = New FamilyTree(bart)
+        Dim secondFamilyTree As FamilyTree = New FamilyTree(marge)
+        firstFamilyTree.Print()
+        Console.WriteLine()
+
+        secondFamilyTree.Print()
+        Console.ReadLine()
+    End Sub
+End Module
