1f053b29252948b7c48ed12a7faf62ed9319193e
Philip Shishov <philshishov@gmail.com>
2021-04-28 13:06:42 +0200

    DefaultIndexer - Indexers and Default property OOP VB.NET

---
+++ src/OOP/DefaultIndexer/MyList.vb
@@ -0,0 +1,34 @@
+﻿Public Class MyList
+
+    Private items(10) As Object
+    Private _count As Integer
+
+    Default Public Property Item(ByVal i As Integer) As Object
+        Get
+            Return items(i)
+        End Get
+        Set(value As Object)
+            items(i) = value
+        End Set
+    End Property
+
+    Public Property Count As Integer
+        Get
+            Return _count
+        End Get
+        Private Set(value As Integer)
+            _count = value
+        End Set
+    End Property
+
+    Public Sub Add(item As Object)
+        items(Count) = item
+        Count += 1
+
+        If Count = items.Length Then
+            Dim longerItems(Count * 2) As Object
+            items.CopyTo(longerItems, 0)
+            items = longerItems
+        End If
+    End Sub
+End Class
---
+++ src/OOP/DefaultIndexer/Program.vb
@@ -0,0 +1,19 @@
+﻿Module Program
+
+    Sub Main()
+        Dim myList As New MyList()
+        myList.Add("First")
+        myList.Add(2)
+
+        For i As Integer = 3 To 99999
+            myList.Add(i)
+        Next
+
+        myList(3) = "Fourth"
+
+        For i As Integer = 0 To 99
+            Console.WriteLine(myList(i))
+        Next
+        Console.ReadKey()
+    End Sub
+End Module
