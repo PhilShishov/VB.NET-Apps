db23a63dfc0879624ec9a88201e445f3f69355ac
Philip Shishov <philshishov@gmail.com>
2021-04-30 13:08:53 +0200

    Delegates OOP VB.NET

---
+++ src/OOP/Delegates/Program.vb
@@ -0,0 +1,13 @@
+﻿Module Program
+    Sub Main()
+        Dim sentence As New Sentence("Delegates in VB .NET work like method references.")
+        sentence.Map(Function(s) If(s.Length > 5, s.ToUpper(), s))
+        Console.WriteLine(sentence)
+        Dim aggregate1 As String = sentence.Aggregate(Function(a, b) a & " " & b)
+        Dim aggregate2 As String = sentence.Aggregate(Function(a, b) "(" & a & ") " & b)
+        Dim aggregate3 As String = sentence.Aggregate(Function(a, b) a & " " & (If(b.Length > 4, b.Substring(0, 3) & ".", b)))
+
+        Console.WriteLine($"Aggregates: {vbCrLf}{aggregate1}{vbCrLf}{aggregate2}{vbCrLf}{aggregate3}")
+        Console.ReadKey()
+    End Sub
+End Module
---
+++ src/OOP/Delegates/Sentence.vb
@@ -0,0 +1,32 @@
+﻿Public Class Sentence
+    Private words() As String
+
+    Public Sub New(text As String)
+        Dim separators = {"."c, ","c, "!"c, ":"c, " "}
+        words = text.Split(separators, StringSplitOptions.RemoveEmptyEntries)
+    End Sub
+
+    Public Delegate Function MapFunction(word As String) As String
+    Public Delegate Function AggregateFunction(result As String, word As String) As String
+
+    Public Sub Map(func As MapFunction)
+        For i As Integer = 0 To words.Length - 1
+            words(i) = func(words(i))
+        Next
+    End Sub
+
+    Public Function Aggregate(func As AggregateFunction) As String
+        If words.Length = 0 Then
+            Return Nothing
+        End If
+        Dim result As String = words(0)
+        For i As Integer = 1 To words.Length - 1
+            result = func(result, words(i))
+        Next
+        Return result
+    End Function
+
+    Public Overrides Function ToString() As String
+        Return String.Join(" ", words) + "."
+    End Function
+End Class
