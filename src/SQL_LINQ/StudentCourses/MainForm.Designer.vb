aec77466b128268cca6c7948d4e1590bce9730cc
Philip Shishov <philshishov@gmail.com>
2021-05-08 23:11:00 +0200

    Student Courses - LINQ to SQL - VB.NET

---
+++ src/SQL_LINQ/StudentCourses/MainForm.Designer.vb
@@ -0,0 +1,102 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.dgvCourses = New System.Windows.Forms.DataGridView()
+        Me.dgvStudents = New System.Windows.Forms.DataGridView()
+        Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
+        CType(Me.dgvCourses, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.dgvStudents, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SplitContainer1.Panel1.SuspendLayout()
+        Me.SplitContainer1.Panel2.SuspendLayout()
+        Me.SplitContainer1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'dgvCourses
+        '
+        Me.dgvCourses.AllowUserToAddRows = False
+        Me.dgvCourses.AllowUserToDeleteRows = False
+        Me.dgvCourses.BackgroundColor = System.Drawing.SystemColors.Control
+        Me.dgvCourses.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvCourses.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.dgvCourses.Location = New System.Drawing.Point(0, 0)
+        Me.dgvCourses.Name = "dgvCourses"
+        Me.dgvCourses.ReadOnly = True
+        Me.dgvCourses.RowHeadersVisible = False
+        Me.dgvCourses.Size = New System.Drawing.Size(385, 300)
+        Me.dgvCourses.TabIndex = 0
+        '
+        'dgvStudents
+        '
+        Me.dgvStudents.AllowUserToAddRows = False
+        Me.dgvStudents.AllowUserToDeleteRows = False
+        Me.dgvStudents.BackgroundColor = System.Drawing.SystemColors.Control
+        Me.dgvStudents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvStudents.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.dgvStudents.Location = New System.Drawing.Point(0, 0)
+        Me.dgvStudents.Name = "dgvStudents"
+        Me.dgvStudents.ReadOnly = True
+        Me.dgvStudents.Size = New System.Drawing.Size(411, 300)
+        Me.dgvStudents.TabIndex = 0
+        '
+        'SplitContainer1
+        '
+        Me.SplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SplitContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.SplitContainer1.Name = "SplitContainer1"
+        '
+        'SplitContainer1.Panel1
+        '
+        Me.SplitContainer1.Panel1.Controls.Add(Me.dgvStudents)
+        '
+        'SplitContainer1.Panel2
+        '
+        Me.SplitContainer1.Panel2.Controls.Add(Me.dgvCourses)
+        Me.SplitContainer1.Size = New System.Drawing.Size(800, 300)
+        Me.SplitContainer1.SplitterDistance = 411
+        Me.SplitContainer1.TabIndex = 2
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 300)
+        Me.Controls.Add(Me.SplitContainer1)
+        Me.Name = "MainForm"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "List Of Students"
+        CType(Me.dgvCourses, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.dgvStudents, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SplitContainer1.Panel1.ResumeLayout(False)
+        Me.SplitContainer1.Panel2.ResumeLayout(False)
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SplitContainer1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents dgvCourses As DataGridView
+    Friend WithEvents dgvStudents As DataGridView
+    Friend WithEvents SplitContainer1 As SplitContainer
+End Class
---
+++ src/SQL_LINQ/StudentCourses/MainForm.vb
@@ -0,0 +1,25 @@
+﻿Public Class MainForm
+    Private db As New CourseRegistrationClassesDataContext
+
+    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim query = From aStudent In db.Students
+                    Select aStudent.Id, aStudent.LastName, aStudent.Status,
+                           aStudent.Major, Courses = aStudent.Courses.Count()
+                    Order By Id
+
+        dgvStudents.DataSource = query
+    End Sub
+
+    Private Sub dgvStudents_SelectionChanged(sender As Object, e As EventArgs) Handles dgvStudents.SelectionChanged
+        If dgvStudents.SelectedRows.Count = 0 Then Return
+
+        Dim studentId As Short = CShort(dgvStudents.SelectedRows(0).Cells(0).Value)
+        Dim query = From aCourse In (From aCourse In db.Courses
+                                     Select aCourse
+                                     Where aCourse.Student_Id = studentId
+                                     Order By aCourse.Id)
+                    Select aCourse.Id, aCourse.Credits, aCourse.Grade
+
+        dgvCourses.DataSource = query
+    End Sub
+End Class
