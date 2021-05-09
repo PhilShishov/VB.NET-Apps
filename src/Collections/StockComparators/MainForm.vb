b9c8de911dfeafd94278e58e1afd11e35a150ff6
Philip Shishov <philshishov@gmail.com>
2021-05-09 22:46:48 +0200

    StockComparators - Collections - VB.NET

---
+++ src/Collections/StockComparators/MainForm.vb
@@ -0,0 +1,54 @@
+﻿Public Class MainForm
+    Private stockList As New List(Of Stock)
+
+    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        stockList.Add(New Stock("MMA", 40, 5))
+        stockList.Add(New Stock("ABC", 80, 20))
+        stockList.Add(New Stock("XYZ", 77, 4))
+        stockList.Add(New Stock("LLT", 43, 5))
+        stockList.Add(New Stock("SBA", 42, 7))
+        DisplayList()
+    End Sub
+
+    Private Sub DisplayList()
+        lstBox.Items.Clear()
+        For Each st As Stock In stockList
+            lstBox.Items.Add(st.ToString())
+        Next
+    End Sub
+
+    Private Sub btnByTicker_Click(sender As Object, e As EventArgs) Handles btnByTicker.Click
+        If chkDescending.Checked Then
+            stockList.Sort()
+            stockList.Reverse()
+        Else
+            stockList.Sort()
+        End If
+
+        DisplayList()
+    End Sub
+
+    Private Sub btnByPrice_Click(sender As Object, e As EventArgs) Handles btnByPrice.Click
+        stockList.Sort(AddressOf CompareByPrice)
+        DisplayList()
+    End Sub
+
+    Private Sub btnByPE_Ratio_Click(sender As Object, e As EventArgs) Handles btnByPE_Ratio.Click
+        stockList.Sort(AddressOf CompareByRatio)
+        DisplayList()
+    End Sub
+
+    Private Function CompareByPrice(x As Stock, y As Stock) As Integer
+        If chkDescending.Checked Then
+            Return y.Price.CompareTo(x.Price)
+        End If
+        Return x.Price.CompareTo(y.Price)
+    End Function
+
+    Private Function CompareByRatio(x As Stock, y As Stock) As Integer
+        If chkDescending.Checked Then
+            Return y.PeRatio.CompareTo(x.PeRatio)
+        End If
+        Return x.PeRatio.CompareTo(y.PeRatio)
+    End Function
+End Class
---
+++ src/Collections/StockComparators/Models/Stock.vb
@@ -0,0 +1,31 @@
+﻿Public Class Stock
+    Implements IComparable
+
+    Public Property Ticker As String
+    Public Property Price As Double
+    Public Property Earnings As Double
+
+    Public ReadOnly Property PeRatio As Double
+        Get
+            If Earnings > 0 Then
+                Return Price / Earnings
+            Else
+                Return 0.0
+            End If
+        End Get
+    End Property
+
+    Public Sub New(pTicker As String, pPrice As Double, pEarnings As Double)
+        Ticker = pTicker
+        Price = pPrice
+        Earnings = pEarnings
+    End Sub
+
+    Public Overrides Function ToString() As String
+        Return Ticker & ", price = " & Price.ToString("n") & ", P/E = " & PeRatio.ToString("n")
+    End Function
+
+    Public Function CompareTo(obj As Object) As Integer Implements IComparable.CompareTo
+        Return Ticker.CompareTo(CType(obj, Stock).Ticker)
+    End Function
+End Class
