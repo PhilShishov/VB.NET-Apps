561d4e164b7b05ca499a28eb393c547170d900b1
Philip Shishov <philshishov@gmail.com>
2021-05-11 13:29:23 +0200

    ClubCommitteeCollection - Collections - VB.NET

--- src/Collections/ClubCommitteeCollection/MainForm.vb
+++ src/Collections/ClubCommitteeCollection/MainForm.vb
@@ -1,4 +1,7 @@
 ﻿Public Class MainForm
+    Private ReadOnly CommiteeCount As Integer = 9
+    Private committees As New List(Of Commitee)
+
     Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
         If cboCommittees.Text.Length = 0 Then
             lblStatus.Text = "Please select a committee"
@@ -8,6 +11,7 @@
         For Each name As String In lstAllMembers.SelectedItems
             If Not lstCommMembers.Items.Contains(name) Then
                 lstCommMembers.Items.Add(name)
+                committees(cboCommittees.SelectedIndex).Items.Add(name)
             End If
         Next
 
@@ -22,10 +26,24 @@
 
         For Each name As String In col
             lstCommMembers.Items.Remove(name)
+            committees(cboCommittees.SelectedIndex).Items.Remove(name)
         Next
     End Sub
 
     Private Sub btnQuit_Click(sender As Object, e As EventArgs) Handles btnQuit.Click
         Close()
+    End Sub
+
+    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        For i As Integer = 0 To cboCommittees.Items.Count - 1
+            committees.Add(New Commitee)
+        Next
+    End Sub
+
+    Private Sub cboCommittees_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboCommittees.SelectedIndexChanged
+        lstCommMembers.Items.Clear()
+        For Each person As String In committees(cboCommittees.SelectedIndex).Items
+            lstCommMembers.Items.Add(person)
+        Next
     End Sub
 End Class
---
+++ src/Collections/ClubCommitteeCollection/Models/Commitee.vb
@@ -0,0 +1,9 @@
+﻿Public Class Commitee
+    Private members As New List(Of String)
+
+    Public ReadOnly Property Items As List(Of String)
+        Get
+            Return members
+        End Get
+    End Property
+End Class
