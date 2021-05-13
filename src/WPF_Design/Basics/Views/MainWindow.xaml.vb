bcc30386f4280ad42dd0e65ea7a785a07c399133
Philip Shishov <philshishov@gmail.com>
2021-05-13 08:45:04 +0200

    Design Basics WPF part 1 - VB.NET

---
+++ src/WPF_Design/Basics/Views/MainWindow.xaml.vb
@@ -0,0 +1,9 @@
+﻿Class MainWindow
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+
+    End Sub
+
+    Private Sub Button_Click_1(sender As Object, e As RoutedEventArgs)
+
+    End Sub
+End Class
